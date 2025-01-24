e895f708c00332c5833d8695c697e31c2f45cdf1
tina <102860264+tinachelwanii@users.noreply.github.com>
2025-01-24 13:21:56 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Anagram/anagram.java
@@ -0,0 +1,40 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.lang.*;
+import java.util.*;
+import java.util.stream.*;
+
+class GFG {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine());
+        while (t-- > 0) {
+            String s1 = br.readLine(); // first string
+            String s2 = br.readLine(); // second string
+
+            Solution obj = new Solution();
+
+            if (obj.areAnagrams(s1, s2)) {
+                System.out.println("true");
+            } else {
+                System.out.println("false");
+            }
+            System.out.println("~");
+        }
+    }
+}
+// } Driver Code Ends
+
+
+class Solution {
+    public static boolean areAnagrams(String s1, String s2) {
+        char[] s1Array=s1.toCharArray();
+        char[] s2Array=s2.toCharArray();
+        Arrays.sort(s1Array);
+        Arrays.sort(s2Array);
+        return Arrays.equals(s1Array,s2Array);
+    }
+}
+    
