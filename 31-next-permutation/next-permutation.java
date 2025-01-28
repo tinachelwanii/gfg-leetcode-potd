cdeb887d6c5350a8921ef51aa663192fc1371f3b
tina <102860264+tinachelwanii@users.noreply.github.com>
2025-01-28 17:20:50 +0530

    Time: 0 ms (100.00%) | Memory: 43.4 MB (21.74%) - LeetSync

--- 31-next-permutation/next-permutation.java
+++ 31-next-permutation/next-permutation.java
@@ -21,12 +21,12 @@ class Solution {
         }
         reverse(nums,gola_index+1,n-1);
     }
-    private void swap(int[] nums, int i, int j) {
+    public void swap(int[] nums, int i, int j) {
         int temp = nums[i];
         nums[i] = nums[j];
         nums[j] = temp;
     }
-    private void reverse(int[] nums, int start, int end) {
+    public void reverse(int[] nums, int start, int end) {
         while (start < end) {
             swap(nums, start, end);
             start++;
