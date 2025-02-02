141372e4e8a6b2272d27ae64ec134c0bd3c570a8
tina <102860264+tinachelwanii@users.noreply.github.com>
2025-02-02 14:46:10 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Search array with adjacent diff at most k/search-array-with-adjacent-diff-at-most-k.java
@@ -0,0 +1,40 @@
+//{ Driver Code Starts
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while (t-- > 0) {
+            int n = sc.nextInt();
+            int[] arr = new int[n];
+            for (int i = 0; i < n; i++) {
+                arr[i] = sc.nextInt();
+            }
+            int k = sc.nextInt();
+            int x = sc.nextInt();
+            Solution sol = new Solution();
+            int ans = sol.findStepKeyIndex(arr, k, x);
+            System.out.println(ans);
+        }
+        sc.close();
+    }
+}
+
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solution {
+    public int findStepKeyIndex(int[] arr, int k, int x) {
+        // code here
+        int n=arr.length;
+        for(int i=0;i<n;i++){
+            if(arr[i]==x){
+                return i;
+            }
+        }
+        return -1;
+    }
+}
