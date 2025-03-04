41e3a107c697e16e07c6fabbd2ab7c196002898e
tina <102860264+tinachelwanii@users.noreply.github.com>
2025-03-04 21:28:41 +0530

    Time: 4 ms (10.17%) | Memory: 43.6 MB (21.12%) - LeetSync

--- 22-generate-parentheses/generate-parentheses.java
+++ 22-generate-parentheses/generate-parentheses.java
@@ -1,21 +1,22 @@
 class Solution {
-    public void generate(List<String> result, int max, int open,int close,String unProcessed) {
-        if(unProcessed.length() == (2*max)) {
-            result.add(unProcessed);
-            return;
-        }
-        
-        if(open<max){
-            generate(result,max, open+1, close, unProcessed+"(");
-        }
-        if(close<open) {
-            generate(result, max, open, close+1, unProcessed+")");
-        }
+    public List<String> generateParenthesis(int n){
+        List<String> ans = new ArrayList<>();
+        solve("", 0, 0, n, ans);
+        return ans;
     }
 
-    public List<String> generateParenthesis(int n) {
-        List<String> result=new ArrayList<String>();
-        generate(result,n,0,0,"");
-        return result;
+    private void solve(String curr, int open, int closed, int total, List<String> ans) {
+        if(curr.length() == 2*total){
+            ans.add(curr);
+            return;
+        }
+
+        if(open<total){
+            solve(curr+"(",open+1,closed,total,ans);
+        }
+
+        if(closed<open){
+            solve(curr+")", open, closed+1, total, ans);
+        }
     }
 }
